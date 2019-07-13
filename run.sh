a1dc235b311608a8640621bb487006c2dc1583e0
Vaibhav <vrongmeal@gmail.com>
2019-07-14 02:48:56 +0530

    Modify run.sh adding colorful logs and exit status.

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+YELLOW="\e[33m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[0m"
+
+printf "${YELLOW}==> Compiling...${RESET}\n"
 g++ main.cpp -o main
-echo 'Program started...'
-echo
+printf "${GREEN}==> Program started${RESET}\n\n"
 ./main
+printf "\n${BLUE}==> Exited with status $?${RESET}\n"
 rm main
